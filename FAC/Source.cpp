176ce734a2a2e3b3c167deffe272aecd60632288
Jirayut Senanorit <araeize_468_arae@hotmail.com>
2020-10-18 23:45:41 +0700

    Add project files.

---
+++ FAC/Source.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void fact(int*);
+void getInt(int*);
+int main()
+{
+	int n;
+	int* pN = &n;
+	getInt(pN);
+	fact(pN);
+
+}
+void fact(int* pN)
+{
+	if (*pN >= 0 && *pN <= 10)
+	{
+		for (int j = *pN;j > 1;j--)
+		{
+			*pN = *pN * (j - 1);
+		}
+		printf("%d", *pN);
+	}
+	else
+	{
+		printf("Error");
+	}
+}
+void getInt(int* pA)
+{
+	scanf_s("%d", pA);
+}
